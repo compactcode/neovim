874e3e998f8f6405b588974e681e4f0de317450a
Shanon McQuay <hi@shan.dog>
2023-01-24 12:49:08 +1100

    feat(lsp): add null-ls
    Makes linters/formatters etc work with lsp tooling.

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -4,6 +4,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.code_actions.gitsigns,
     null_ls.builtins.formatting.jq,
+    null_ls.builtins.formatting.nixfmt,
     null_ls.builtins.hover.printenv,
   },
 })
